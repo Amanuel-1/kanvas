97ea5909c421eb754310aedc91b6daefff248f52
amanuel-1 <amanuelgaromsa@gmail.com>
2024-09-16 15:24:13 +0300

    fix:minor fix

--- src/app/(studio)/_components/brand.tsx
+++ src/app/(studio)/_components/brand.tsx
@@ -4,7 +4,7 @@ const Brand = () => {
   return (
     <div className='flex gap-3 items-center px-4'>
       <div className='w-8 h-8  rounded-full bg-stone-700' />
-      <p className="text-xl font-sans">ogiz</p>
+      <p className="text-xl font-sans">kanvas</p>
 
     </div>
   )
